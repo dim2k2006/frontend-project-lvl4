a54ada4a559859d5ec9a1c7d57a7e113d7d911d6
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-16 22:24:16 +0300

    Update reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,11 @@
 import { createReducer } from '@reduxjs/toolkit';
+import produce from 'immer';
+import * as actions from '../actions';
 
 const reducer = createReducer({}, {
-
+  [actions.activateChannel]: produce((draft, action) => {
+    draft.activeChannel = action.payload.channel;
+  }),
 });
 
 export const getChannels = (state) => state.channels;
