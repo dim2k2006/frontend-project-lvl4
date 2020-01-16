dc505420a0103d10eb41049399cc1ff1f27da9d0
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-16 22:24:41 +0300

    Update "Layout" component

--- src/ui/components/Layout/index.jsx
+++ src/ui/components/Layout/index.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import flow from 'lodash/flow';
+import { connect } from 'react-redux';
 import Aside from '../Aside/index';
+import * as actions from '../../../redux/actions';
 
-const Layout = () => (
-  <div className="container-fluid h-100">
-    <div className="row h-100">
-      <div className="col-4 col-sm-4 col-md-3 col-lg-3 col-xl-2  h-100 bg-info">
-        <Aside />
+const Layout = ({ activateChannel }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    activateChannel({ channel: location.pathname });
+  });
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row h-100">
+        <div className="col-4 col-sm-4 col-md-3 col-lg-3 col-xl-2  h-100 bg-info">
+          <Aside />
+        </div>
+
+        <div className="col-8 col-sm-8 col-md-9 col-lg-9 col-xl-10 h-100" />
       </div>
-
-      <div className="col-8 col-sm-8 col-md-9 col-lg-9 col-xl-10 h-100" />
     </div>
-  </div>
-);
+  );
+};
 
-export default Layout;
+export default flow(
+  connect(
+    null,
+    actions,
+  ),
+)(Layout);
