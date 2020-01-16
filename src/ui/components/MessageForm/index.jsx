e48cea535f328178afe6e0e080b1652d7aa255da
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-16 23:50:33 +0300

    Add "MessageForm" component

---
+++ src/ui/components/MessageForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MessageForm = () => (
+  <form className="w-100 p-3">
+    <textarea className="form-control w-100" id="message" placeholder="Enter Your Message" />
+  </form>
+);
+
+export default MessageForm;
