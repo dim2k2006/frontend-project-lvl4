7515437300c4e7bd2e6d466a585f951f7b0474ee
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-18 20:10:42 +0300

    Update "MessageForm" component

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,4 +2,42 @@ import { createAction } from '@reduxjs/toolkit';
 
 export const activateChannel = createAction('CHANNEL_ACTIVATE');
 
-export const testAction = createAction('TEST_ACTION');
+export const submitMessageRequest = createAction('MESSAGE_SUBMIT_REQUEST');
+export const submitMessageSuccess = createAction('MESSAGE_SUBMIT_SUCCESS');
+export const submitMessageFailure = createAction('MESSAGE_SUBMIT_FAILURE');
+
+export const submitMessage = (data, endpoint, form) => (dispatch) => {
+  dispatch(submitMessageRequest());
+
+  // return axios({
+  //   method: 'POST',
+  //   url: `${host}${endpoint}`,
+  //   data,
+  //   headers,
+  // })
+  //   .then(() => {
+  //     dispatch(submitMessageSuccess());
+  //
+  //     // setTimeout(() => {
+  //     //   dispatch(reset(form));
+  //     //   dispatch(submitCustomerFormReset());
+  //     // }, resetDelay);
+  //   })
+  //   .catch((error) => {
+  //     // const status = get(error, 'response.status', 500);
+  //     // const errors = get(error, 'response.data.errors', {});
+  //     // const errorMessage = keys(errors)
+  //     //   .reduce((accumulator, key) => ({
+  //     //     ...accumulator,
+  //     //     [key]: errors[key].join(', '),
+  //     //   }), {});
+  //     //
+  //     // if (status === 422) {
+  //     //   throw new SubmissionError(errorMessage);
+  //     // }
+  //
+  //     dispatch(submitMessageFailure());
+  //
+  //     // throw error;
+  //   });
+};
--- src/ui/components/MessageForm/index.jsx
+++ src/ui/components/MessageForm/index.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import flow from 'lodash/flow';
+import get from 'lodash/get';
+import { connect } from 'react-redux';
+import { Formik } from 'formik';
+import * as actions from '../../../redux/actions';
 
-const MessageForm = () => (
-  <form className="w-100 p-3">
-    <textarea className="form-control w-100" id="message" placeholder="Enter Your Message" />
-  </form>
+const MessageForm = ({ onSubmit }) => (
+  <Formik
+    initialValues={{ message: '' }}
+    onSubmit={onSubmit}
+  >
+    {(props) => (
+      <div
+        role="presentation"
+        onKeyDown={(event) => {
+          const message = get(props, 'values.message', '');
+
+          if (!message.length || event.keyCode !== 13) return;
+
+          event.preventDefault();
+
+          props.handleSubmit();
+        }}
+      >
+        <form
+          onSubmit={props.handleSubmit}
+          className="w-100 p-3"
+        >
+          <textarea
+            name="message"
+            className="form-control w-100"
+            placeholder="Enter Your Message"
+            value={props.values.name}
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+          />
+        </form>
+      </div>
+    )}
+  </Formik>
 );
 
-export default MessageForm;
+MessageForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default flow(
+  connect(
+    null,
+    (dispatch) => ({
+      onSubmit: (data) => {
+        console.log('data:', data);
+      },
+    }),
+  ),
+)(MessageForm);
