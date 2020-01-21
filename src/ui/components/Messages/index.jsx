a170321917c2ee48731029d670aca886cef79430
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-21 22:42:00 +0300

    Update "Messages" component

--- src/ui/components/Messages/index.jsx
+++ src/ui/components/Messages/index.jsx
@@ -1,41 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import flow from 'lodash/flow';
+import { connect } from 'react-redux';
 import Message from '../Message';
+import { getMessages } from '../../../redux/reducers';
 
-const messages = [
-  {
-    id: 1,
-    name: 'Alex',
-    time: '12:00 AM',
-    text: 'Here goes some text',
-  },
-  {
-    id: 2,
-    name: 'Alex',
-    time: '12:00 AM',
-    text: 'Here goes some text',
-  },
-  {
-    id: 3,
-    name: 'Alex',
-    time: '12:00 AM',
-    text: 'Here goes some text',
-  },
-  {
-    id: 4,
-    name: 'Alex',
-    time: '12:00 AM',
-    text: 'Here goes some text',
-  },
-];
-
-const Messages = () => (
+const Messages = ({ messages }) => (
   <div className="w-100 flex-grow-1 p-3 overflow-auto">
     {
       messages.map((message) => (
         <Message
           key={message.id}
-          name={message.name}
-          time={message.time}
+          name={message.author}
+          time={message.date}
           text={message.text}
         />
       ))
@@ -43,4 +20,18 @@ const Messages = () => (
   </div>
 );
 
-export default Messages;
+Messages.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.object),
+};
+
+Messages.defaultProps = {
+  messages: [],
+};
+
+export default flow(
+  connect(
+    (state) => ({
+      messages: getMessages(state),
+    }),
+  ),
+)(Messages);
