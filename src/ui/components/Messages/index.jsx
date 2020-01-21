b5ecb2d9bc2da5bbf99dca48235625c999f8a9c2
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-21 22:53:13 +0300

    Update "Messages" component

--- src/ui/components/Messages/index.jsx
+++ src/ui/components/Messages/index.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import flow from 'lodash/flow';
 import { connect } from 'react-redux';
 import Message from '../Message';
 import { getMessages } from '../../../redux/reducers';
 
-const Messages = ({ messages }) => (
-  <div className="w-100 flex-grow-1 p-3 overflow-auto">
-    {
-      messages.map((message) => (
-        <Message
-          key={message.id}
-          name={message.author}
-          time={message.date}
-          text={message.text}
-        />
-      ))
-    }
-  </div>
-);
+const Messages = ({ messages }) => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    container.current.scrollTo(0, 1e10);
+  });
+
+  return (
+    <div ref={container} className="w-100 flex-grow-1 p-3 overflow-auto">
+      {
+        messages.map((message) => (
+          <Message
+            key={message.id}
+            name={message.author}
+            time={message.date}
+            text={message.text}
+          />
+        ))
+      }
+    </div>
+  );
+};
 
 Messages.propTypes = {
   messages: PropTypes.arrayOf(PropTypes.object),
