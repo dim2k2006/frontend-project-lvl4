76f452a7bd6df9bde4570582e550ca1f897ee794
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-24 10:36:18 +0300

    Update "Aside" component

--- src/ui/components/Aside/index.jsx
+++ src/ui/components/Aside/index.jsx
@@ -3,11 +3,24 @@ import flow from 'lodash/flow';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Octicon, { Plus } from '@primer/octicons-react';
 import { getChannels, getUserName, getActiveChannel } from '../../../redux/reducers';
 
 const Aside = ({ channels, userName, activeChannel }) => (
   <div className="pt-4">
-    <h6 className="text-white">{userName}</h6>
+    <div className="mb-4">
+      <h3 className="text-white mb-0">Hexlet:slack</h3>
+
+      <small className="text-white">{userName}</small>
+    </div>
+
+    <h5 className="text-white position-relative">
+      Channels
+
+      <button type="button" className="position-absolute btn btn-link text-white p-0 pl-1 pr-1" style={{ right: '0', top: '50%', transform: 'translateY(-50%)' }}>
+        <Octicon icon={Plus} />
+      </button>
+    </h5>
 
     <ul className="list-group list-group-flush">
       {
@@ -15,9 +28,14 @@ const Aside = ({ channels, userName, activeChannel }) => (
           const Tag = channel.id === activeChannel ? 'strong' : 'span';
 
           return (
-            <li key={channel.id} className="list-group-item bg-transparent">
-              <Link to={`/channels/${channel.id}`}>
-                <button type="button" className="btn btn-link text-white text-capitalize"><Tag>{channel.name}</Tag></button>
+            <li key={channel.id} className="list-group-item bg-transparent pl-0 pt-0 pb-0 border-bottom-0">
+              <Link to={`/channels/${channel.id}`} className="d-block">
+                <button type="button" className="btn btn-link text-decoration-none text-white text-capitalize p-0">
+                  <Tag>
+                    #
+                    {channel.name}
+                  </Tag>
+                </button>
               </Link>
             </li>
           );
