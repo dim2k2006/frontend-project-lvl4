563f8a40a8e53ffe1ff34eeab7f3d0871fc36922
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-01-28 22:11:24 +0300

    Update "Modal" component

--- src/ui/components/Modal/index.jsx
+++ src/ui/components/Modal/index.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Portal from '../Portal';
 
 const Modal = ({ title, children, onClose }) => (
-  <div
-    className="modal fade"
-    tabIndex="-1"
-    role="dialog"
-  >
-    <div className="modal-dialog" role="document">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">{title}</h5>
+  <Portal id="modal">
+    <div
+      className="modal fade"
+      tabIndex="-1"
+      role="dialog"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
 
-          <button
-            type="button"
-            className="close"
-            data-dismiss="modal"
-            aria-label="Close"
-            onClick={onClose}
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={onClose}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
 
-        <div className="modal-body">
-          {children}
+          <div className="modal-body">
+            {children}
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </Portal>
 );
 
 Modal.propTypes = {
