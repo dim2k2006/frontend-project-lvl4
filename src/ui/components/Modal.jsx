85d2c0ae3b2837011d232175d41f73400457444d
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-02-23 11:28:41 +0300

    Update "Modal" component

--- src/ui/components/Modal.jsx
+++ src/ui/components/Modal.jsx
@@ -1,52 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import flow from 'lodash/flow';
-import { connect } from 'react-redux';
-import * as actions from '../../redux/actions';
+import { useDispatch } from 'react-redux';
+import { actions } from '../../redux/slices';
 
-const Modal = ({ title, children, resetModal }) => (
-  <>
-    <div
-      className="modal fade show d-block"
-      tabIndex="-1"
-      role="dialog"
-    >
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
+const Modal = ({ title, children }) => {
+  const dispatch = useDispatch();
 
-            <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
-              onClick={() => resetModal()}
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
+  return (
+    <>
+      <div
+        className="modal fade show d-block"
+        tabIndex="-1"
+        role="dialog"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{title}</h5>
 
-          <div className="modal-body">
-            {children}
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={() => dispatch(actions.resetModal())}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <div className="modal-body">
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="modal-backdrop fade show" />
-  </>
-);
+      <div className="modal-backdrop fade show" />
+    </>
+  );
+};
 
 Modal.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  resetModal: PropTypes.func.isRequired,
 };
 
-export default flow(
-  connect(
-    null,
-    actions,
-  ),
-)(Modal);
+export default Modal;
