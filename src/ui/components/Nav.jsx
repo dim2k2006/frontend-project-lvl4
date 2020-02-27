25fda5fbf841c9e7a8f4ba6c0929ab2011a0ea34
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-02-27 22:52:48 +0300

    Update "Nav" component

--- src/ui/components/Nav.jsx
+++ src/ui/components/Nav.jsx
@@ -1,67 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import flow from 'lodash/flow';
 import get from 'lodash/get';
 import find from 'lodash/find';
-import { withProps } from 'recompose';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getChannels } from '../../redux/slices/channels';
 import { getActiveChannel } from '../../redux/slices/activeChannel';
 import { actions } from '../../redux/slices';
 
-const Nav = ({
-  channelName,
-  removable,
-  removeChannel,
-  editChannel,
-}) => (
-  <nav className="navbar navbar-light bg-light border-bottom w-100 d-flex justify-content-between">
-    <h5 className="text-capitalize mb-0">{channelName}</h5>
+const Nav = () => {
+  const channels = useSelector(getChannels);
+  const activeChannel = useSelector(getActiveChannel);
+  const channel = find(channels, (ch) => ch.id === activeChannel);
+  const channelName = get(channel, 'name', '');
+  const removable = get(channel, 'removable', false);
+  const dispatch = useDispatch();
 
-    <div>
-      <button
-        type="button"
-        className="btn btn-secondary btn-sm"
-        onClick={() => editChannel()}
-      >
-        Edit
-      </button>
+  return (
+    <nav className="navbar navbar-light bg-light border-bottom w-100 d-flex justify-content-between">
+      <h5 className="text-capitalize mb-0">{channelName}</h5>
 
-      {removable && (
+      <div>
         <button
           type="button"
-          className="btn btn-danger btn-sm ml-2"
-          onClick={() => removeChannel()}
+          className="btn btn-secondary btn-sm"
+          onClick={() => dispatch(actions.editChannel())}
         >
-          Remove
+          Edit
         </button>
-      )}
-    </div>
-  </nav>
-);
 
-Nav.propTypes = {
-  channelName: PropTypes.string.isRequired,
-  removable: PropTypes.bool.isRequired,
-  removeChannel: PropTypes.func.isRequired,
-  editChannel: PropTypes.func.isRequired,
+        {removable && (
+          <button
+            type="button"
+            className="btn btn-danger btn-sm ml-2"
+            onClick={() => dispatch(actions.removeChannel())}
+          >
+            Remove
+          </button>
+        )}
+      </div>
+    </nav>
+  );
 };
 
-export default flow(
-  withProps((props) => {
-    const channels = get(props, 'channels');
-    const activeChannel = get(props, 'activeChannel');
-    const channel = find(channels, (ch) => ch.id === activeChannel);
-    const channelName = get(channel, 'name', '');
-    const removable = get(channel, 'removable', false);
-
-    return { channelName, removable };
-  }),
-  connect(
-    (state) => ({
-      channels: getChannels(state),
-      activeChannel: getActiveChannel(state),
-    }),
-    actions,
-  ),
-)(Nav);
+export default Nav;
