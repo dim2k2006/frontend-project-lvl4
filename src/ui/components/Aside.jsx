e6143ff4f3c8988fe68f42dda3d944d7dcad8549
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-02-27 22:34:43 +0300

    Update "Aside" component

--- src/ui/components/Aside.jsx
+++ src/ui/components/Aside.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Octicon, { Plus } from '@primer/octicons-react';
-import { getChannels } from '../../redux/reducers';
+import { getChannels } from '../../redux/slices/channels';
 import { getActiveChannel } from '../../redux/slices/activeChannel';
 import { getUserName } from '../../redux/slices/userName';
 import { actions } from '../../redux/slices';
