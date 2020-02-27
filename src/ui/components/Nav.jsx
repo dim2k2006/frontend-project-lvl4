7c9ef4d78df6ab89b8c3be149186c7968924c1cc
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-02-27 22:35:11 +0300

    Update "Nav" component

--- src/ui/components/Nav.jsx
+++ src/ui/components/Nav.jsx
@@ -5,7 +5,7 @@ import get from 'lodash/get';
 import find from 'lodash/find';
 import { withProps } from 'recompose';
 import { connect } from 'react-redux';
-import { getChannels } from '../../redux/reducers';
+import { getChannels } from '../../redux/slices/channels';
 import { getActiveChannel } from '../../redux/slices/activeChannel';
 import { actions } from '../../redux/slices';
 
