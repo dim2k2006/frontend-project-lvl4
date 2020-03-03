a0b103feab53940ddeecaef9a69dcdc4fbd44bd7
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-03 23:38:29 +0300

    Update "Messages" component

--- src/ui/components/Messages.jsx
+++ src/ui/components/Messages.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import Message from './Message';
 import { getActiveChannel } from '../../redux/slices/activeChannel';
 import { getMessagesForChannel } from '../../redux/slices/messages';
 
@@ -16,13 +15,25 @@ const Messages = () => {
   return (
     <div ref={container} className="w-100 flex-grow-1 p-3 overflow-auto">
       {
-        messages.map((message) => (
-          <Message
-            key={message.id}
-            name={message.author}
-            time={message.date}
-            text={message.text}
-          />
+        messages.map(({
+          id,
+          author,
+          date,
+          text,
+        }) => (
+          <div key={id} className="card mb-4 w-100 border-0">
+            <div className="row no-gutters">
+              <div className="col-sm-12">
+                <div className="card-body p-0">
+                  <h5 className="card-title mb-1">{author}</h5>
+
+                  <p className="card-text mb-1">{text}</p>
+
+                  <p className="card-text"><small className="text-muted">{date}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
         ))
       }
     </div>
