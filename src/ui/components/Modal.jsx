dd56486bd53ad8444d409774a95ac651e8a401d0
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-05 23:36:03 +0300

    Update "Modal" component

--- src/ui/components/Modal.jsx
+++ src/ui/components/Modal.jsx
@@ -1,49 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { actions } from '../../redux/slices';
+import connect from '../../connect';
 
-const Modal = ({ title, children }) => {
-  const dispatch = useDispatch();
+const Modal = ({ title, hideModal, children }) => (
+  <>
+    <div
+      className="modal fade show d-block"
+      tabIndex="-1"
+      role="dialog"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
 
-  return (
-    <>
-      <div
-        className="modal fade show d-block"
-        tabIndex="-1"
-        role="dialog"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">{title}</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={hideModal}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
 
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-                onClick={() => dispatch(actions.hideModal())}
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-
-            <div className="modal-body">
-              {children}
-            </div>
+          <div className="modal-body">
+            {children}
           </div>
         </div>
       </div>
+    </div>
 
-      <div className="modal-backdrop fade show" />
-    </>
-  );
-};
+    <div className="modal-backdrop fade show" />
+  </>
+);
 
 Modal.propTypes = {
   title: PropTypes.string.isRequired,
+  hideModal: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 };
 
-export default Modal;
+export default connect()(Modal);
