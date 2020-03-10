b55e3cd317cfd5acafa48e669d1113ac4e38a702
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-10 22:34:41 +0300

    Update "MessageForm" component

--- src/ui/components/MessageForm.jsx
+++ src/ui/components/MessageForm.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
-import curry from 'lodash/curry';
 import { useSelector } from 'react-redux';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import { getUserName } from '../../redux/slices/userName';
 import { getMessageSubmittingState } from '../../redux/slices/messageSubmittingState';
 import { getActiveChannel } from '../../redux/slices/activeChannel';
@@ -13,67 +12,64 @@ const MessageForm = ({ submitMessage }) => {
   const userName = useSelector(getUserName);
   const activeChannel = useSelector(getActiveChannel);
   const submittingState = useSelector(getMessageSubmittingState);
-  const onSubmit = (values, { resetForm }) => {
-    const text = get(values, 'message');
-    const data = {
-      data: {
-        attributes: {
-          text,
-          author: userName,
+  const formik = useFormik({
+    initialValues: { message: '' },
+    onSubmit: (values, { resetForm }) => {
+      const text = get(values, 'message');
+      const data = {
+        data: {
+          attributes: {
+            text,
+            author: userName,
+          },
         },
-      },
-    };
+      };
 
-    submitMessage(activeChannel, data, resetForm);
-  };
-  const onKeyDown = curry((props, event) => {
-    const message = get(props, 'values.message', '');
+      submitMessage(activeChannel, data, resetForm);
+    },
+  });
+
+  const onKeyDown = (event) => {
+    const message = get(formik, 'values.message', '');
 
     if (!message.length || event.keyCode !== 13) return;
 
     event.preventDefault();
 
-    props.handleSubmit();
-  });
+    formik.handleSubmit();
+  };
 
   return (
-    <Formik
-      initialValues={{ message: '' }}
-      onSubmit={onSubmit}
+    <div
+      role="presentation"
+      onKeyDown={onKeyDown}
     >
-      {(props) => (
-        <div
-          role="presentation"
-          onKeyDown={onKeyDown(props)}
-        >
-          <form
-            onSubmit={props.handleSubmit}
-            className="w-100 p-3 position-relative"
-          >
-            <textarea
-              name="message"
-              className="form-control w-100"
-              placeholder="Enter Your Message"
-              value={props.values.message}
-              onChange={props.handleChange}
-              onBlur={props.handleBlur}
-              disabled={submittingState === 'requested'}
-            />
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-100 p-3 position-relative"
+      >
+        <textarea
+          name="message"
+          className="form-control w-100"
+          placeholder="Enter Your Message"
+          value={formik.values.message}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          disabled={submittingState === 'requested'}
+        />
 
-            {submittingState === 'requested' && (
-              <div className="fixed-top position-absolute w-100 h-100 d-flex align-items-center justify-content-center">
-                <div
-                  className="spinner-border text-info"
-                  role="status"
-                >
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            )}
-          </form>
-        </div>
-      )}
-    </Formik>
+        {submittingState === 'requested' && (
+          <div className="fixed-top position-absolute w-100 h-100 d-flex align-items-center justify-content-center">
+            <div
+              className="spinner-border text-info"
+              role="status"
+            >
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
