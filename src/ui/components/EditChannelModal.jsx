8754ec5afb998f39d4cede93f357cd1a984a0f91
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-10 22:38:42 +0300

    Update "EditChannelModal" component

--- src/ui/components/EditChannelModal.jsx
+++ src/ui/components/EditChannelModal.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import get from 'lodash/get';
 import find from 'lodash/find';
 import { useSelector } from 'react-redux';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import Modal from './Modal';
 import { getChannels } from '../../redux/slices/channels';
 import { getActiveChannel } from '../../redux/slices/activeChannel';
@@ -16,53 +16,49 @@ const EditChannelModal = ({ updateChannel }) => {
   const channels = useSelector(getChannels);
   const activeChannelData = find(channels, (channel) => channel.id === activeChannel);
   const channelName = get(activeChannelData, 'name');
-  const onSubmit = (values, { resetForm }) => {
-    const name = get(values, 'name');
-    const data = {
-      data: {
-        attributes: {
-          name,
+  const formik = useFormik({
+    initialValues: { name: channelName },
+    onSubmit: (values, { resetForm }) => {
+      const name = get(values, 'name');
+      const data = {
+        data: {
+          attributes: {
+            name,
+          },
         },
-      },
-    };
+      };
 
-    updateChannel(activeChannel, data, resetForm);
-  };
+      updateChannel(activeChannel, data, resetForm);
+    },
+  });
 
   return (
     <Modal title="Edit channel">
-      <Formik
-        initialValues={{ name: channelName }}
-        onSubmit={onSubmit}
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-100 p-3 position-relative"
       >
-        {(props) => (
-          <form
-            onSubmit={props.handleSubmit}
-            className="w-100 p-3 position-relative"
-          >
-            <div className="form-group">
-              <input
-                type="text"
-                name="name"
-                className="form-control w-100"
-                placeholder="Enter Channel name"
-                value={props.values.name}
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                disabled={channelEditingState === 'requested'}
-              />
-            </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            className="form-control w-100"
+            placeholder="Enter Channel name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            disabled={channelEditingState === 'requested'}
+          />
+        </div>
 
-            <button className="btn btn-primary" type="submit" disabled={channelEditingState === 'requested'}>
-              {channelEditingState === 'requested' && (
-                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
-              )}
+        <button className="btn btn-primary" type="submit" disabled={channelEditingState === 'requested'}>
+          {channelEditingState === 'requested' && (
+            <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+          )}
 
-              Edit channel
-            </button>
-          </form>
-        )}
-      </Formik>
+          Edit channel
+        </button>
+      </form>
     </Modal>
   );
 };
