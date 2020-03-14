1ca666b71829d312d88f3898ea1b37bcb580cca4
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-14 11:28:24 +0300

    Add facade for selectors

--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -1,20 +1,20 @@
 import { combineReducers } from 'redux';
 
-import messageSubmittingState, { actions as messageSubmittingStateActions } from './messageSubmittingState';
-import messages, { actions as messagesActions } from './messages';
+import messageSubmittingState, { actions as messageSubmittingStateActions, getMessageSubmittingState } from './messageSubmittingState';
+import messages, { actions as messagesActions, getMessages, getMessagesForChannel } from './messages';
 
-import channelAddingState, { actions as channelAddingStateActions } from './channelAddingState';
-import channelRemovingState, { actions as channelRemovingStateActions } from './channelRemovingState';
-import channelEditingState, { actions as channelEditingStateActions } from './channelEditingState';
-import channels, { actions as channelsActions } from './channels';
+import channelAddingState, { actions as channelAddingStateActions, getChannelAddingState } from './channelAddingState';
+import channelRemovingState, { actions as channelRemovingStateActions, getChannelRemovingState } from './channelRemovingState';
+import channelEditingState, { actions as channelEditingStateActions, getChannelEditingState } from './channelEditingState';
+import channels, { actions as channelsActions, getChannels } from './channels';
 
-import modalState, { actions as modalStateActions } from './modalState';
+import modalState, { actions as modalStateActions, getModalState } from './modalState';
 
-import activeChannel, { actions as activeChannelActions } from './activeChannel';
+import activeChannel, { actions as activeChannelActions, getActiveChannel } from './activeChannel';
 
-import errorMessage, { actions as errorMessageActions } from './errorMessage';
+import errorMessage, { actions as errorMessageActions, getErrorMessage } from './errorMessage';
 
-import userName from './userName';
+import userName, { getUserName } from './userName';
 
 export default combineReducers({
   messageSubmittingState,
@@ -42,3 +42,21 @@ const actions = {
 };
 
 export { actions };
+
+const selectors = {
+  activeChannel: getActiveChannel,
+  channelAddingState: getChannelAddingState,
+  channelEditingState: getChannelEditingState,
+  channelRemovingState: getChannelRemovingState,
+  channels: getChannels,
+  errorMessage: getErrorMessage,
+  messages: getMessages,
+  messagesForChannel: getMessagesForChannel,
+  messageSubmittingState: getMessageSubmittingState,
+  modalState: getModalState,
+  userName: getUserName,
+};
+
+const getSelector = (type) => selectors[type];
+
+export { getSelector };
