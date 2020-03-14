2c0103b6a6878eeb7ecc8175c5442715d627394e
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-14 12:18:54 +0300

    Add i18n

---
+++ src/i18n.js
@@ -0,0 +1,15 @@
+import i18next from 'i18next';
+
+i18next.init({
+  lng: 'en',
+  resources: {
+    en: {
+      translation: {
+        ADD_CHANNEL: 'Something went wrong during creating the channel. Please try again.',
+
+      },
+    },
+  },
+});
+
+export default i18next;
